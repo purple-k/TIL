f08df879122267d263581ca322efca5c2d196bae
bora <eus03017@naver.com>
2026-01-23 01:30:49 +0900

    feat: add chapter02 LLM API를 사용하여 챗봇 만들기

---
+++ agent/yozm-ai-agent/chapter02/chatbot.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+
+
+def chatbot_response(user_message: str):
+    result = client.responses.create(model="gpt-5-mini", input=user_message)
+    return result
+
+
+if __name__ == "__main__":
+    while True:
+        user_message = input("메시지: ")
+        if user_message.lower() == "exit":
+            print("대화를 종료합니다.")
+            break
+
+        result = chatbot_response(user_message)
+        print("챗봇: ", result.output_text)
---
+++ agent/yozm-ai-agent/chapter02/chatbot2.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+
+
+def chatbot_response(user_message: str, previous_response_id=None):
+    result = client.responses.create(model="gpt-5-mini", input=user_message, previous_response_id=previous_response_id)
+    return result
+
+
+if __name__ == "__main__":
+    previous_response_id = None
+    while True:
+        user_message = input("메시지: ")
+        if user_message.lower() == "exit":
+            print("대화를 종료합니다.")
+            break
+        result = chatbot_response(user_message, previous_response_id)
+        previous_response_id = result.id
+        print(result.output_text)
